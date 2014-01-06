2bd7c89dee1538dca2f157e81bd7c5088e899bc9
mrolnik <rolnik.mika@gmail.com>
2014-01-06 18:45:16 -0300

    asd

--- public/javascripts/logSocial.js
+++ public/javascripts/logSocial.js
@@ -13,11 +13,10 @@ $(document).on('click', '.fb.login', function(e) {
 	FB.login(function(response) {
 		if (response.authResponse) {
 			console.log('Welcome!  Fetching your information.... ');
+			document.cookie = "fb-log=true";
 			FB.api('/me', function(response) {
 				console.log('Good to see you, ' + response.name + '.');
 			});
-		} else {
-			console.log('User cancelled login or did not fully authorize.');
 		}
 	}, {scope: 'read_stream, publish_stream'});
 });
